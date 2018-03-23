0ea0aea8177d85a6d682a8bcdc855e54c2188d0a
Salvalai Fabio <fabio@seesharp.ch>
2018-03-23 15:12:58 +0100

    fix paths

--- dotnet-versioning/define-version-variables-appveyor.ps1
+++ dotnet-versioning/define-version-variables-appveyor.ps1
@@ -8,9 +8,9 @@ param (
 )
 
 # Find setted version
-$version = & '.\ic-tools\.NET Versioning\GetVersionDotnetCore.ps1' $csprojPath
+$version = & '.\GetVersionDotnetCore.ps1' $csprojPath
 # Decide if the version should be changed
-$updatedVersion = & '.\ic-tools\.NET Versioning\DefineNextVersion.ps1' $version $packageName $branchName 'DummyParameterBecauseImSureThisIsUseless'
+$updatedVersion = & '.\DefineNextVersion.ps1' $version $packageName $branchName 'DummyParameterBecauseImSureThisIsUseless'
 
 # get the version suffix (ex: beta01) if any
 $separatorPosition = $updatedVersion.IndexOf("-") + 1 
