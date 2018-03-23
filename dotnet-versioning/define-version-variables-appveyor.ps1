732ad1f73fa4e5ad6ba7e2bca4f7c9125071b655
Salvalai Fabio <fabio@seesharp.ch>
2018-03-23 17:09:23 +0100

    improve feedback in the messages window

--- dotnet-versioning/define-version-variables-appveyor.ps1
+++ dotnet-versioning/define-version-variables-appveyor.ps1
@@ -14,6 +14,7 @@ $version = & "$scriptsPath\GetVersionDotnetCore.ps1" $csprojPath
 
 if (!$version)
 {
+    # The following line is appveyor-specific.
     Add-AppveyorMessage -Message "failed to read assembly version in $csprojPath" -Category Error -Details "Make sure the $csprojPath file contains a Project.PropertyGroup.AssemblyVersion node."
     exit 1
 }
@@ -23,8 +24,13 @@ $updatedVersion = & "$scriptsPath\DefineNextVersion.ps1" $version $packageName $
 
 # get the version suffix (ex: beta01) if any
 $separatorPosition = $updatedVersion.IndexOf("-") + 1 
-$suffix = $updatedVersion.Substring($separatorPosition)
+if ($separatorPosition -gt 0)
+{
+    $suffix = $updatedVersion.Substring($separatorPosition)
+}
 
-# Only the following lines are appveyor-specific.
+# The following lines are appveyor-specific.
 Set-AppveyorBuildVariable "suffix" $suffix
 Set-AppveyorBuildVariable "version" $updatedVersion
+Add-AppveyorMessage -Message "assembly version detected: $updatedVersion" -Category Information 
+Add-AppveyorMessage -Message "suffix version detected: $suffix" -Category Information 
